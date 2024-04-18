ae7b5bce64ddb5c47cb12af4348c256bc8d858b0
Lane Rettig <lanerettig@gmail.com>
2024-04-18 15:42:59 -0700

    WIP

--- client/client.ts
+++ client/client.ts
@@ -1,156 +1,112 @@
-import * as web3 from "@solana/web3.js";
 import * as anchor from "@coral-xyz/anchor";
-import {
-  getAccount,
-  getOrCreateAssociatedTokenAccount,
-} from "@solana/spl-token";
-import { PublicKey } from "@solana/web3.js";
-import { keypairIdentity, token, Metaplex } from "@metaplex-foundation/js";
-import type { TokenVault } from "../target/types/token_vault";
+import {initializeKeypair} from "./initializeKeypair"
+import {getAccount, getOrCreateAssociatedTokenAccount,} from "@solana/spl-token";
+import {PublicKey} from "@solana/web3.js";
+import {keypairIdentity, Metaplex, token} from "@metaplex-foundation/js";
+import type {TokenVault} from "../target/types/token_vault";
+import {TokenStandard} from "@metaplex-foundation/mpl-token-metadata";
+
+const program = anchor.workspace.TokenVault as anchor.Program<TokenVault>;
+// const keypair = anchor.web3.Keypair.generate();
+// const mintAuthority = (program.provider as anchor.AnchorProvider).wallet
+const decimals = 9;
 
 // Configure the client to use the local cluster
 anchor.setProvider(anchor.AnchorProvider.env());
 
-const program = anchor.workspace.TokenVault as anchor.Program<TokenVault>;
+async function main() {
+    const user = await initializeKeypair(program.provider.connection);
+    // const user = await initializeKeypair(anchor.getProvider().connection);
+    console.log(user.publicKey.toBase58())
 
+    let [tokenAccountOwnerPda] = PublicKey.findProgramAddressSync(
+        [Buffer.from("token_account_owner_pda")],
+        program.programId
+    );
 
-const mintAuthority = program.provider.wallet.payer;
+    const metaplex = new Metaplex(program.provider.connection).use(
+        keypairIdentity(user)
+    );
 
-const decimals = 9;
+    const createdSFT = await metaplex.nfts().createSft({
+        uri: "https://storage.googleapis.com/lane-misc/spudcoinmeta",
+        name: "SpudCoin",
+        symbol: "SPUD",
+        sellerFeeBasisPoints: 0,
+        updateAuthority: user,
+        mintAuthority: user,
+        decimals: decimals,
+        tokenStandard: TokenStandard.Fungible,
+        isMutable: true,
+    });
 
-let [tokenAccountOwnerPda] = PublicKey.findProgramAddressSync(
-  [Buffer.from("token_account_owner_pda")],
-  program.programId
-);
+    console.log(
+        "Creating semi fungible spl token with address: " + createdSFT.sft.address
+    );
 
-const metaplex = new Metaplex(program.provider.connection).use(
-  keypairIdentity(program.provider.wallet.payer)
-);
+    const mintDecimals = Math.pow(10, decimals);
 
-const createdSFT = await metaplex.nfts().createSft({
-  uri: "https://shdw-drive.genesysgo.net/AzjHvXgqUJortnr5fXDG2aPkp2PfFMvu4Egr57fdiite/PirateCoinMeta",
-  name: "Gold",
-  symbol: "GOLD",
-  sellerFeeBasisPoints: 100,
-  updateAuthority: mintAuthority,
-  mintAuthority: mintAuthority,
-  decimals: decimals,
-  tokenStandard: "Fungible",
-  isMutable: true,
-});
+    let mintResult = await metaplex.nfts().mint({
+        nftOrSft: createdSFT.sft,
+        authority: user,
+        toOwner: user.publicKey,
+        amount: token(100 * mintDecimals),
+    });
 
-console.log(
-  "Creating semi fungible spl token with address: " + createdSFT.sft.address
-);
+    console.log("Mint to result: " + mintResult.response.signature);
 
-const mintDecimals = Math.pow(10, decimals);
+    const tokenAccount = await getOrCreateAssociatedTokenAccount(
+        program.provider.connection,
+        user,
+        createdSFT.mintAddress,
+        user.publicKey,
+    );
 
-let mintResult = await metaplex.nfts().mint({
-  nftOrSft: createdSFT.sft,
-  authority: program.provider.wallet.payer,
-  toOwner: program.provider.wallet.payer.publicKey,
-  amount: token(100 * mintDecimals),
-});
+    console.log("tokenAccount: " + tokenAccount.address);
+    console.log("TokenAccountOwnerPda: " + tokenAccountOwnerPda);
 
-console.log("Mint to result: " + mintResult.response.signature);
+    let tokenAccountInfo = await getAccount(program.provider.connection, tokenAccount.address);
+    console.log(
+        "Owned token amount: " + tokenAccountInfo.amount / BigInt(mintDecimals)
+    );
+    // let [tokenVault] = PublicKey.findProgramAddressSync(
+    //     [Buffer.from("token_vault"), createdSFT.mintAddress.toBuffer()],
+    //     program.programId
+    // );
+    // console.log("VaultAccount: " + tokenVault);
+    //
+    let confirmOptions = {
+        skipPreflight: true,
+    };
 
-const tokenAccount = await getOrCreateAssociatedTokenAccount(
-  program.provider.connection,
-  program.provider.wallet.payer,
-  createdSFT.mintAddress,
-  program.provider.wallet.payer.publicKey
-);
+    let txHash = await program.methods
+        .initialize()
+        .accounts({
+            tokenAccountOwnerPda: tokenAccountOwnerPda,
+            // vaultTokenAccount: tokenVault,
+            senderTokenAccount: tokenAccount.address,
+            mintOfTokenBeingSent: createdSFT.mintAddress,
+            signer: program.provider.publicKey,
+        })
+        .rpc(confirmOptions);
 
-console.log("tokenAccount: " + tokenAccount.address);
-console.log("TokenAccountOwnerPda: " + tokenAccountOwnerPda);
+    console.log(`Initialize`);
+    await logTransaction(txHash);
 
-let tokenAccountInfo = await getAccount(program.provider.connection, tokenAccount.address);
-console.log(
-  "Owned token amount: " + tokenAccountInfo.amount / BigInt(mintDecimals)
-);
-let [tokenVault] = PublicKey.findProgramAddressSync(
-  [Buffer.from("token_vault"), createdSFT.mintAddress.toBuffer()],
-  program.programId
-);
-console.log("VaultAccount: " + tokenVault);
+    // console.log(`Vault initialized.`);
+    console.log(
+        "Owned token amount: " + tokenAccountInfo.amount / BigInt(mintDecimals)
+    );
+    // tokenAccountInfo = await getAccount(program.provider.connection, tokenVault);
+    // console.log(
+    //     "Vault token amount: " + tokenAccountInfo.amount / BigInt(mintDecimals)
+    // );
 
-let confirmOptions = {
-  skipPreflight: true,
-};
-
-let txHash = await program.methods
-  .initialize()
-  .accounts({
-    tokenAccountOwnerPda: tokenAccountOwnerPda,
-    vaultTokenAccount: tokenVault,
-    senderTokenAccount: tokenAccount.address,
-    mintOfTokenBeingSent: createdSFT.mintAddress,
-    signer: program.provider.publicKey,
-  })
-  .rpc(confirmOptions);
-
-console.log(`Initialize`);
-await logTransaction(txHash);
-
-console.log(`Vault initialized.`);
-tokenAccountInfo = await getAccount(program.provider.connection, tokenAccount.address);
-console.log(
-  "Owned token amount: " + tokenAccountInfo.amount / BigInt(mintDecimals)
-);
-tokenAccountInfo = await getAccount(program.provider.connection, tokenVault);
-console.log(
-  "Vault token amount: " + tokenAccountInfo.amount / BigInt(mintDecimals)
-);
-
-txHash = await program.methods
-  .transferIn(new anchor.BN(1 * mintDecimals))
-  .accounts({
-    tokenAccountOwnerPda: tokenAccountOwnerPda,
-    vaultTokenAccount: tokenVault,
-    senderTokenAccount: tokenAccount.address,
-    mintOfTokenBeingSent: createdSFT.mintAddress,
-    signer: program.provider.publicKey,
-  })
-  .signers([program.provider.wallet.payer])
-  .rpc(confirmOptions);
-
-console.log(`Transfer one token into the vault.`);
-await logTransaction(txHash);
-
-tokenAccountInfo = await getAccount(program.provider.connection, tokenAccount.address);
-console.log(
-  "Owned token amount: " + tokenAccountInfo.amount / BigInt(mintDecimals)
-);
-
-tokenAccountInfo = await getAccount(program.provider.connection, tokenVault);
-console.log(
-  "Vault token amount: " + tokenAccountInfo.amount / BigInt(mintDecimals)
-);
-
-txHash = await program.methods
-  .transferOut(new anchor.BN(1 * mintDecimals))
-  .accounts({
-    tokenAccountOwnerPda: tokenAccountOwnerPda,
-    vaultTokenAccount: tokenVault,
-    senderTokenAccount: tokenAccount.address,
-    mintOfTokenBeingSent: createdSFT.mintAddress,
-    signer: program.provider.publicKey,
-  })
-  .signers([program.provider.wallet.payer])
-  .rpc(confirmOptions);
-
-console.log(`Transfer one token out of the vault.`);
-await logTransaction(txHash);
-
-tokenAccountInfo = await getAccount(program.provider.connection, tokenAccount.address);
-console.log(
-  "Owned token amount: " + tokenAccountInfo.amount / BigInt(mintDecimals)
-);
-
-tokenAccountInfo = await getAccount(program.provider.connection, tokenVault);
-console.log(
-  "Vault token amount: " + tokenAccountInfo.amount / BigInt(mintDecimals)
-);
+    // tokenAccountInfo = await getAccount(program.provider.connection, tokenAccount.address);
+    // console.log(
+    //     "Owned token amount: " + tokenAccountInfo.amount / BigInt(mintDecimals)
+    // );
+}
 
 async function logTransaction(txHash) {
   const { blockhash, lastValidBlockHeight } =
--- programs/token-vault/src/lib.rs
+++ programs/token-vault/src/lib.rs
@@ -11,18 +11,6 @@ mod token_vault {
     pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
-
-    pub fn transfer_in(_ctx: Context<TransferAccounts>, amount: u64) -> Result<()> {
-        msg!("Token amount transfer in: {}!", amount);
-        // See Tutorial page 6
-        Ok(())
-    }
-
-    pub fn transfer_out(_ctx: Context<TransferAccounts>, amount: u64) -> Result<()> {
-        msg!("Token amount transfer out: {}!", amount);
-        // See Tutorial page 7
-        Ok(())
-    }
 }
 
 #[derive(Accounts)]
@@ -37,15 +25,15 @@ pub struct Initialize<'info> {
     )]
     token_account_owner_pda: AccountInfo<'info>,
 
-    #[account(
-        init_if_needed,
-        payer = signer,
-        seeds=[b"token_vault", mint_of_token_being_sent.key().as_ref()],
-        token::mint=mint_of_token_being_sent,
-        token::authority=token_account_owner_pda,
-        bump
-    )]
-    vault_token_account: Account<'info, TokenAccount>,
+    // #[account(
+    //     init_if_needed,
+    //     payer = signer,
+    //     seeds=[b"token_vault", mint_of_token_being_sent.key().as_ref()],
+    //     token::mint=mint_of_token_being_sent,
+    //     token::authority=token_account_owner_pda,
+    //     bump
+    // )]
+    // vault_token_account: Account<'info, TokenAccount>,
 
     mint_of_token_being_sent: Account<'info, Mint>,
 
@@ -65,13 +53,13 @@ pub struct TransferAccounts<'info> {
     )]
     token_account_owner_pda: AccountInfo<'info>,
 
-    #[account(mut,
-        seeds=[b"token_vault", mint_of_token_being_sent.key().as_ref()],
-        bump,
-        token::mint=mint_of_token_being_sent,
-        token::authority=token_account_owner_pda,
-    )]
-    vault_token_account: Account<'info, TokenAccount>,
+    // #[account(mut,
+    //     seeds=[b"token_vault", mint_of_token_being_sent.key().as_ref()],
+    //     bump,
+    //     token::mint=mint_of_token_being_sent,
+    //     token::authority=token_account_owner_pda,
+    // )]
+    // vault_token_account: Account<'info, TokenAccount>,
 
     #[account(mut)]
     sender_token_account: Account<'info, TokenAccount>,
@@ -83,47 +71,4 @@ pub struct TransferAccounts<'info> {
     system_program: Program<'info, System>,
     token_program: Program<'info, Token>,
     rent: Sysvar<'info, Rent>,
-}
-
-pub fn transfer_in(ctx: Context<TransferAccounts>, amount: u64) -> Result<()> {
-    msg!("Token amount transfer in: {}!", amount);
-    
-    let transfer_instruction = Transfer {
-        from: ctx.accounts.sender_token_account.to_account_info(),
-        to: ctx.accounts.vault_token_account.to_account_info(),
-        authority: ctx.accounts.signer.to_account_info(),
-    };
-
-    let cpi_ctx = CpiContext::new(
-        ctx.accounts.token_program.to_account_info(),
-        transfer_instruction,
-    );
-
-    anchor_spl::token::transfer(cpi_ctx, amount)?;
-
-    Ok(())
-}
-
-pub fn transfer_out(ctx: Context<TransferAccounts>, amount: u64) -> Result<()> {
-    msg!("Token amount transfer out: {}!", amount);
-
-    let transfer_instruction = Transfer {
-        from: ctx.accounts.vault_token_account.to_account_info(),
-        to: ctx.accounts.sender_token_account.to_account_info(),
-        authority: ctx.accounts.token_account_owner_pda.to_account_info(),
-    };
-
-    let bump = ctx.bumps.token_account_owner_pda;
-    let seeds = &[b"token_account_owner_pda".as_ref(), &[bump]];
-    let signer = &[&seeds[..]];
-
-    let cpi_ctx = CpiContext::new_with_signer(
-        ctx.accounts.token_program.to_account_info(),
-        transfer_instruction,
-        signer,
-    );
-
-    anchor_spl::token::transfer(cpi_ctx, amount)?;
-
-    Ok(())
 }
